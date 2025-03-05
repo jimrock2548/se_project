f96a009002457a8fa80c3adfcb49539beb7bc1c4
Sornsaran <s6604062620328@email.kmutnb.ac.th>
2025-03-06 01:07:33 +0700

    do-backend

--- apps/web/src/app/page.jsx
+++ apps/web/src/app/page.jsx
@@ -1,80 +1,45 @@
-import Link from "next/link";
-
-function Page() {
+export default function Home() {
   return (
     <>
-      <div className='flex justify-center mt-5'>
-        <div className='sm:px-10 md:px-20 lg:px-40'>
-          <Link href='/Payment'>
-          <button className="flex bg-base-100 flex-col items-center justify-center border-2 border-red-300 rounded-xl text-center p-7 px-20 shadow-lg hover:bg-gradient-to-r hover:from-red-200 hover:to-blue-200 transition duration-300">
-            <h1 className="text-lg sm:text-3xl font-bold text-red-500">
-              <span className="mr-2"></span>ค่าเช่าห้องพัก
-            </h1>
-            <h2 className="text-lg sm:text-xl font-bold text-red-500">ยอดค้างชำระรวม</h2>
-            <p className="text-2xl sm:text-3xl font-bold text-red-500 my-1">4,000</p>
-            <span className="badge badge-error badge-outline text-xs sm:text-sm px-2 py-0.5 mt-1">
-              1/2/2567
-            </span>
-            <span className=" font-bold text-red-500 my-1">(กดปุ่มนี้เพื่อชำระค่าห้องเช่า)</span>
-          </button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="flex justify-center mt-5">
-        <div role="tablist" className="tabs tabs-lifted w-full max-w-[1800px] mx-auto text-lg pb-32">
-          {/* Tab 1 */}
-          <input
-            type="radio"
-            name="my_tabs_2"
-            role="tab"
-            className="tab text-lg hover:underline hover:text-red-500"
-            aria-label="Announcement"
-          />
-          <div
-            role="tabpanel"
-            className="tab-content bg-base-100 border-base-300 rounded-box p-6 text-lg shadow-lg"
-          >
-            <div className='mb-4 flex items-center text-base-content'>
-              <span className="mr-2">🎄</span>Merry Christmas: มีขนมแจกฟรีที่ lobby ช่วยกินหน่อย
-            </div>
-            
-          </div>
-
-          {/* Tab 2 */}
-          <input
-            type="radio"
-            name="my_tabs_2"
-            role="tab"
-            className="tab text-lg hover:underline hover:text-red-500"
-            aria-label="Tab 2"
-            defaultChecked
-          />
-          <div
-            role="tabpanel"
-            className="tab-content bg-base-100 text-base-content border-base-300 rounded-box p-6 text-lg shadow-lg"
-          >
-            📝 รายละเอียดเพิ่มเติมใน Tab 2
-          </div>
-
-          {/* Tab 3 */}
-          <input
-            type="radio"
-            name="my_tabs_2"
-            role="tab"
-            className="tab text-lg hover:underline hover:text-red-500"
-            aria-label="Tab 3"
-          />
-          <div
-            role="tabpanel"
-            className="tab-content bg-base-100 text-base-content border-base-300 rounded-box p-6 text-lg shadow-lg"
-          >
-            📦 ข้อมูล Tab 3
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)" }}
+      >
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card bg-base-100 bg-opacity-50 w-full max-w-sm shrink-0 shadow-2xl m-56 border-1">
+            <form className="card-body">
+              <div className="form-control">
+                <h1 className="text-center text-3xl font-bold pb-5 text-black">Login</h1>
+                <label className="label">
+                  <span className="label-text text-black">Email</span>
+                </label>
+                <input type="email" placeholder="Email / Username" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black">Password</span>
+                </label>
+                <input type="password" placeholder="password" className="input input-bordered" required />
+                <label className="label pt-4">
+                  <label className="label-text-alt text-black">
+                    <input type="checkbox"/> Remember me
+                  </label>
+                  <a href="#" className="label-text-alt link link-hover text-black">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn glass text-black">Login</button>
+              </div>
+              <div className="text-center label-text-alt py-2">
+                <h3 className=" text-black">
+                  Don't have an account?
+                  <a href="/Register" className="font-bold text-black ps-1">Register</a>
+                </h3>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     </>
   );
 }
-
-export default Page;
